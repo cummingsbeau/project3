c45f6b52cd9ba1784b820ae88895becf98b3d4c2
Beau <cummingsbeau@gmail.com>
2019-05-03 13:59:32 -0700

    fixed hook call error

---
+++ client/build/precache-manifest.26e70b2701f9393cc117dde8ebeb1a5a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "dcb085f99c898f669c48493e2f4ead37",
+    "url": "/static/media/bungee-shade-latin-400.dcb085f9.woff"
+  },
+  {
+    "revision": "93569efcabf27c9230e9b94f6dd9e338",
+    "url": "/static/media/bungee-shade-latin-400.93569efc.woff2"
+  },
+  {
+    "revision": "98863ae6bf834748050cbf12691c6340",
+    "url": "/static/media/background-blur-category-545331.98863ae6.jpg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b0ffb578ec0286d2a10c",
+    "url": "/static/js/main.12dc8bf1.chunk.js"
+  },
+  {
+    "revision": "e6a36b59f0598dac2815",
+    "url": "/static/js/2.d8b25038.chunk.js"
+  },
+  {
+    "revision": "b0ffb578ec0286d2a10c",
+    "url": "/static/css/main.0830b0bb.chunk.css"
+  },
+  {
+    "revision": "e6a36b59f0598dac2815",
+    "url": "/static/css/2.56abf400.chunk.css"
+  },
+  {
+    "revision": "42b3e0b735375542e3de1c5a27daca27",
+    "url": "/index.html"
+  }
+];
